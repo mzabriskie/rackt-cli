439ad155f917dc6debcba3331d0ef76c6185e9d7
Julien Bouquillon <julien@bouquillon.com>
2015-10-09 17:42:04 +0200

    refacto pages task :
     - dont rm and reinstall node_modules, exploit git index
     - publish examples folder on a fresh branch
     - create an index from examples directory

---
+++ tasks/getExamplesIndex.js
@@ -0,0 +1,29 @@
+#!../node_modules/.bin/babel-node
+
+import fs from 'fs';
+import path from 'path';
+
+function generateIndex({pkg, examples=[]}={}) {
+  // build example list
+  const examplesList = examples.map(example => {
+    return `<li><a href=\"./examples/${example}/index.html\">${example}</a></li>`;
+  }).join('\n');
+
+  return `
+    <title>${pkg.name} Examples</title>
+    <h1>${pkg.name} Examples</h1>
+    <h3><a href="${pkg.homepage}">${pkg.homepage}</a></h3>
+    <p>${pkg.description}</p>
+    <ul>
+      ${examplesList}
+    </ul>
+  `;
+}
+
+const pkg = require(path.join(process.cwd(), 'package.json'));
+const examples = fs.readdirSync(path.join(process.cwd(), 'examples')).filter(name => (name !== '__build__'));
+
+// output to stdout
+const finalHtml = generateIndex({ pkg, examples });
+
+console.log(finalHtml);
